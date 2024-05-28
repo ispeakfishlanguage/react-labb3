ba313d743ef8f94bbc41d8e56b04cefc236c3cf3
Carolina Leguizamon <63667103+ispeakfishlanguage@users.noreply.github.com>
2024-05-28 21:49:45 +0200

    Add MovieListItem component

---
+++ components/MovieListItem.jsx
@@ -0,0 +1,24 @@
+// components/MovieListItem.js
+import Link from 'next/link'
+
+export default function MovieListItem({ movie }) {
+  return (
+    <div>
+      <Link href={`/movie/${movie.id}`}>
+        <a>
+          <h3>{movie.title}</h3>
+        </a>
+      </Link>
+      <p>{movie.overview.substring(0, 100)}...</p>
+      <style jsx>{`
+        div {
+          margin-bottom: 1rem;
+        }
+        a {
+          text-decoration: none;
+          color: inherit;
+        }
+      `}</style>
+    </div>
+  )
+}
